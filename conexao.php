182dc2bacdc9ae2343109da636c6645f0d8311b4
Fábio Oliveira <fabiodeoliveirabr@gmail.com>
2024-11-01 18:32:34 -0300

    Iniciado

--- conexao.php
+++ conexao.php
@@ -3,9 +3,10 @@ $maquina = "localhost";
 $usuario = "root";
 $senha = "root";
 $nome_do_banco = "bd_sistema_senai2";
+$porta = 3306;
 
 try {
-    $pdo = new PDO("mysql:host=$maquina;dbname=$nome_do_banco;charset=utf8", "$usuario", "$senha");
+    $pdo = new PDO("mysql:host=$maquina;port=$porta;dbname=$nome_do_banco;charset=utf8", "$usuario", "$senha");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Conectado com sucesso";
 } catch (PDOException $erro) {
--- pesquisar.php
+++ pesquisar.php
@@ -21,10 +21,10 @@
             $categoria = $item['categoria'];
             echo "
                   <div class='cartoes'>
-                        <h3>Nº $codigo</h3>
-                        <p>$descricao</p>
-                        <p>Setor: $setor</p>
-                        <p>Categoria: $categoria</p>
+                        <h3>Nº <span class='cod_produto'>$codigo</span></h3>
+                        <p><span class='desc_produto'>$descricao</span></p>
+                        <p>Setor: <span class='setor_produto'>$setor</span></p>
+                        <p>Categoria: <span class='cat_produto'>$categoria</span></p>
                   </div>                         
             ";
         }
